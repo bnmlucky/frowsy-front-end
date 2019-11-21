80951c0d72bd00df7f417db571318275c95d9a0c
Guadalupe Ramirez <guadaluperamirez@Guadalupes-MacBook-Pro.local>
2019-11-21 12:23:00 -0500

    adding front end components and app.js content

---
+++ frowsy-front-end/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import "./App.css";
+import CreateNew from "./components/CreateNew";
+import MainContent from "./components/MainContent";
+import Update from "./components/Update";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //PROPERTIES FROM THE SCHEMS?
+    };
+    //BINDING STATEMENTS
+  }
+  componentDidMount() {
+    //WE WILL HAVE A CALL FROM THE BACK-END
+  }
+  //ASYNC CALLS
+
+  //RENDER/RETURN
+  render() {
+    return (
+      <div>
+        <MainContent />
+        <CreateNew />
+        <Update />
+      </div>
+    );
+  }
+}
+
+export default App;
