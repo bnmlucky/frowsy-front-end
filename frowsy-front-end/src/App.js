a2a3393c237916bb3bcd8e23869d257c83112f21
Natalia Titova <bnmlucky@gmail.com>
2019-11-21 15:14:52 -0500

    Removed Register and Login buttons

--- frowsy-front-end/src/App.js
+++ frowsy-front-end/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 import MainContent from "./components/MainContent";
-import NewUser from "./components/NewUser";
 import axios from "axios";
 
 class App extends Component {
@@ -16,17 +15,12 @@ class App extends Component {
   //WE WILL HAVE A CALL FROM THE BACK-END
   // }
   //ASYNC CALLS --
-  handleAddUser(user) {
-    console.log(user);
-  }
+
   //RENDER/RETURN
   render() {
     return (
       <div className="appJs-main-div">
         <h2>Hello</h2>
-        <button>Register</button>
-        <button>Login</button>
-        <NewUser handleAddUser={this.handleAddUser} />
         <MainContent />
       </div>
     );
