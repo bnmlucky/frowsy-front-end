7338c7a11078b98c41a4b525dda09cd0b48ccd03
Natalia Titova <bnmlucky@gmail.com>
2019-11-21 15:38:58 -0500

    got a component for the task showing up in ToDo list not connected to data, though

--- frowsy-front-end/src/components/MainContent.js
+++ frowsy-front-end/src/components/MainContent.js
@@ -2,28 +2,31 @@ import React, { Component } from "react";
 import axios from "axios";
 import CreateNew from "../components/CreateNew";
 import Update from "../components/Update";
+import Task from './Task.js'
 
 class MainContent extends Component {
-  constructor() {
+    constructor() {
         super()
         this.state = {
 
         }
     }
-  
-  render() {
-    return (
-      <div className="mainContent-main-div">
-        <h1>Frowsy</h1>
-      <h2>Main Content is here</h2>
-        <div className="ToDo">To Do:</div>
-        <div className="Doing">Doing:</div>
-        <div className="Done">Done:</div>
-        <br />
-        <CreateNew />
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="mainContent-main-div">
+                <h1>Frowsy</h1>
+                <h2>Main Content is here</h2>
+                <div className="ToDo">To Do:
+            <Task />
+                </div>
+                <div className="Doing">Doing:</div>
+                <div className="Done">Done:</div>
+                <br />
+                <CreateNew />
+            </div>
+        );
+    }
 }
 
 export default MainContent;
