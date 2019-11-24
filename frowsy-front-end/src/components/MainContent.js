0cb63cd492617831853a2a7a5ec92daaccaeb05c
Alice D'Arcangelo <alicedarcangelo@Alices-MacBook-Pro.local>
2019-11-24 11:40:54 -0500

    front end deleting working

--- frowsy-front-end/src/components/MainContent.js
+++ frowsy-front-end/src/components/MainContent.js
@@ -3,55 +3,69 @@ import axios from "axios";
 import CreateNew from "../components/CreateNew";
 import Update from "../components/Update";
 import Task from "./Task.js";
-
+const baseURL = "http://localhost:3003";
 class MainContent extends Component {
-    constructor() {
-        super();
-        this.state = {
-            user: "",
-            loggedIn: true,
-            tasks: []
-        };
-        this.handleAddTask = this.handleAddTask.bind(this)
-    }
-    handleAddTask(task) {
-        console.log(task)
-        this.setState({ tasks: [...this.state.tasks, task] })
+  constructor() {
+    super();
+    this.state = {
+      user: "",
+      loggedIn: true,
+      tasks: []
+    };
+    this.handleAddTask = this.handleAddTask.bind(this);
+    this.handleDeleteTask = this.handleDeleteTask.bind(this);
+  }
+  handleAddTask(task) {
+    console.log(task);
+    this.setState({ tasks: [...this.state.tasks, task] });
+  }
+  async handleDeleteTask(task) {
+    const userID = await localStorage.getItem("user");
+    const taskID = task._id;
 
-    }
-    componentDidMount() {
-        const tasks = this.props.tasks;
-        this.setState({ task: tasks, user: this.props.userid });
-    }
+    const response = await axios.delete(`${baseURL}/tasks/${userID}/${taskID}`);
 
-    render() {
-        return (
-            <main>
-                <CreateNew user={this.state.user} getTasks={this.props.getTasks} handleAddTask={this.handleAddTask} />
-                <br />
-                <div className="container">
-                    To Do:
-                {this.props.tasks.map(task => {
-                        return (
-                            <div className="ToDo" key={task._id}>
-                                <ul>
-                                    <li>
-                                        {task.description} &nbsp;
-                                     <button>&#128465;</button> &nbsp;
-                                     <button>✎</button> &nbsp;
-                             </li>
-                                </ul>
-                            </div>
-                        )
-                    }
-                    )}
-                    <div className="Doing">Doing:</div>
-                    <div className="Done">Done:</div>
-                    <br />
-                </div>
-            </main >
-        );
-    }
+    this.props.getTasks();
+  }
+  componentDidMount() {
+    const tasks = this.props.tasks;
+    this.setState({ task: tasks, user: this.props.userid });
+  }
+
+  render() {
+    return (
+      <main>
+        <CreateNew
+          user={this.state.user}
+          getTasks={this.props.getTasks}
+          handleAddTask={this.handleAddTask}
+        />
+        <br />
+        <div className="container">
+          To Do:
+          {this.props.tasks.map(task => {
+            return (
+              <div className="ToDo" key={task._id}>
+                <ul>
+                  <li>
+                    {task.description} &nbsp;
+                    <button onClick={() => this.handleDeleteTask(task)}>
+                      &#128465;
+                    </button>{" "}
+                    &nbsp;
+                    <button>✎</button> &nbsp;
+                  </li>
+                </ul>
+              </div>
+            );
+          })}
+          <div className="Doing">Doing:</div>
+          <div className="Done">Done:</div>
+          <br />
+        </div>
+      </main>
+    );
+  }
 }
 
 export default MainContent;
