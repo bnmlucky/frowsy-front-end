9122a3f1e804a77a4412a83e9d17021981672e58
Alice D'Arcangelo <alicedarcangelo@Alices-MacBook-Pro.local>
2019-11-26 11:03:49 -0500

    added some content to frowsy front page

--- frowsy-front-end/src/components/Home.js
+++ frowsy-front-end/src/components/Home.js
@@ -3,8 +3,18 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     return (
-      <div>
-        <p>About Frowsy:</p>
+      <div className="home">
+        <p className=" mx-auto w-75">
+          <h1 className="bold">
+            {" "}
+            FROWSY lets you work more efficiently and get more done.
+          </h1>
+          <span className=" w-50">
+            {" "}
+            Frowsy’s boards and lists enable you to organize and prioritize your
+            projects in a fun, flexible, and rewarding way.
+          </span>
+        </p>
       </div>
     );
   }
